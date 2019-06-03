77da3fc43c173e39eb329db857c65550f4ff9d60
rldhkstopic <rldhks6899@naver.com>
2019-06-03 23:00:58 +0900

    update severals

--- Detect_eye.py
+++ Detect_eye.py
@@ -2,6 +2,8 @@ from scipy.spatial import distance as dist
 from imutils.video import FileVideoStream
 from imutils.video import VideoStream
 from imutils import face_utils
+
+import RPi.GPIO as gpio
 import argparse
 import imutils
 import time
@@ -9,78 +11,72 @@ import dlib
 import cv2
 
 
+cnt = 0
+total = 0
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 def eye_aspect_ratio(eye):
-	A = dist.euclidean(eye[1], eye[5])
-	B = dist.euclidean(eye[2], eye[4])
-	C = dist.euclidean(eye[0], eye[3])
-	ear = (A + B) / (2.0 * C)
-	return ear
+	return (dist.euclidean(eye[1], eye[5]) + dist.euclidean(eye[2], eye[4]))/(dist.euclidean(eye[0], eye[3])*2)
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", default="shape_predictor.dat",
-	help="path to facial landmark predictor")
-ap.add_argument("-t", "--threshold", type=float, default=0.2,
-	help="threshold to determine closed eyes")
-ap.add_argument("-f", "--frames", type=int, default=2,
-	help="the number of consecutive frames the eye must be below the threshold")
+def arg_parse():
+	parse = argparse.ArgumentParser()
+	parse.add_argument("-p", dest = 'predict' default="shape_predictor.dat", help+"shape predictor file")
+	parse.add_argument("-t", dest = 'threshold', default=0.2, type=float)
+	parse.add_argument("-f", dest = 'frames',default=2, type=int)
+	return parse.parse_args()
 
-args = vars(ap.parse_args())
-EYE_AR_THRESH = args['threshold']
-EYE_AR_CONSEC_FRAMES = args['frames']
+def grayscale(image):
+	 return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-COUNTER = 0
-TOTAL = 0
+def eyes(srt, end):
+	EYE = shape[srt:end]
+	EAR = eye_aspect_ratio(EYE)
+	return EYE, EAR
 
 print("[INFO] loading facial landmark predictor...")
+eye_thresh = args.threshold
+eye_frames = args.frames
+eye_predict = args.predict
+
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor.dat")
+predictor = dlib.shape_predictor(eye_predict)
 
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+(l_srt, l_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(r_srt, r_end) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
+width, height = cap.read.shape[:2]
+whalf, hhalf = int(width/2), int(height/2)
+
 while cap.isOpened():
-	ret, frame = cap.read()
-	width, height = frame.shape[:2]
-	whalf, hhalf = int(width/2), int(height/2)
+	_, frame = cap.read()
+	gray = grayscale(frame)
 
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	rects = detector(gray, 0)
+	for rect in detector(gray, 0):
+		shape = face_utils.shape_to_np(predictor(gray, rect))
 
-	for rect in rects:
-		shape = predictor(gray, rect)
-		shape = face_utils.shape_to_np(shape)
+		l_eye, l_ear = eyes(l_srt, l_end)
+		r_eye, r_ear = eyes(r_srt, r_end)
+		m_ear = (l_ear + r_ear) / 2.0
+		print(m_ear, end = "\b\b\b\b\b")
 
-		leftEye = shape[lStart:lEnd]
-		leftEAR = eye_aspect_ratio(leftEye)
-		rightEye = shape[rStart:rEnd]
-		rightEAR = eye_aspect_ratio(rightEye)
+		cv2.drawContours(frame, [cv2.convexHull(l_eye)], -1, (0, 255, 0), 1, maxLevel=1)
+		cv2.drawContours(frame, [cv2.convexHull(r_eye)], -1, (0, 255, 0), 1, maxLevel=1)
 
-		ear = (leftEAR + rightEAR) / 2.0
-
-		cv2.drawContours(frame, [cv2.convexHull(leftEye)], -1, (0, 255, 0), 1, maxLevel=1)
-		cv2.drawContours(frame, [cv2.convexHull(rightEye)], -1, (0, 255, 0), 1, maxLevel=1)
-		# Threshold만큼 눈이 감기면 Blink로 간주
-		if ear < EYE_AR_THRESH:
-			COUNTER += 1
-
-			if COUNTER > 60:
+		if ear < eye_thresh:
+			cnt += 1
+			if cnt > 60:
 				cv2.putText(frame, "Dont Sleep", (whalf, hhalf), font, 1, (0, 0, 255), 2)
-				print("Dont Sleep")
+				print("\nDont Sleep")
 		else:
-			if COUNTER >= EYE_AR_CONSEC_FRAMES:
-				TOTAL += 1
-
-			COUNTER = 0
-
-		cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30), font, 0.7, (0, 0, 255), 2)
-		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30), font, 0.7, (0, 0, 255), 2)
+			if cnt >= eye_frames:
+				total += 1
+			cnt = 0
+		# cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30), font, 0.7, (0, 0, 255), 2)
 
 	cv2.imshow("Frame", frame)
 	if cv2.waitKey(1) & 0xFF == ord("q"):
 		break
 
 cap.release()
-cap.stop()
 cv2.destroyAllWindows()
